1ae8c06427cefbedc68a54da332ff5594144d914
Ayuhamidah <ayu.hamidah97@gmail.com>
2018-12-09 15:25:32 +0700

    Ini adalah contoh program login menggunakan mysql

---
+++ src/pbol_bab8loginakun/Pbol_bab8LoginAkun.java
@@ -0,0 +1,9 @@
+package pbol_bab8loginakun;
+public class Pbol_bab8LoginAkun {
+
+    public static void main(String[] args) {
+        FormLogin F = new FormLogin();
+        F.setVisible(true);
+    }
+    
+}
